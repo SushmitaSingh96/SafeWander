0f3647ff90b0c1443a6494720bdb17651a4c09ea
SushmitaSingh96 <sushmitasingh1996@gmail.com>
2025-07-01 15:07:58 +0200

    modified bash script

--- keep_awake.sh
+++ keep_awake.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-while true; do
-  curl -s https://safe-wander-backend.onrender.com/health > /dev/null
-  echo "Pinged at $(date)"
-  sleep 600  # ping every 10mins
-done
+curl -s https://safe-wander-backend.onrender.com/health > /dev/null
+echo "Pinged at $(date)"
